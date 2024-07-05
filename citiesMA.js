d52a0383bb6c3d462a8f12a6b275c5595be696dc
MD <medouladmoussa@gmail.com>
2024-07-05 04:33:05 +0100

    Morocco Cities

---
+++ citiesMA.js
@@ -0,0 +1,102 @@
+const cities = [
+	{
+	nameAR : 'أكادير',
+	name : "Agadir-Ida-Ou-Tanane"
+	},
+	{
+	nameAR : 'الحسيمة',
+	name : "Al Hoceïma"
+	},
+	{
+	nameAR : 'الدار البيضاء',
+	name : "Casablanca"
+	},
+	{
+	nameAR : 'شفشاون',
+	name : "Chefchaouen"
+	},
+	{
+	nameAR : 'الداخلة',
+	name : "Dakhla"
+	},
+	{
+	nameAR : 'الجديدة',
+	name : "El Jadida"
+	},
+	{
+	nameAR : 'الراشيدية',
+	name : "Errachidia"
+	},
+	{
+	nameAR : 'الصويرة',
+	name : "Essaouira"
+	},
+	{
+	nameAR : 'فاس',
+	name : "Fès"
+	},
+	{
+	nameAR : 'القنيطرة',
+	name : "Kénitra"
+	},
+	{
+	nameAR : 'خريبكة',
+	name : "Khouribga"
+	},
+	{
+	nameAR : 'العيون',
+	name : "Laâyoune"
+	},
+	{
+	nameAR : 'العرائش',
+	name : "Larache"
+	},
+	{
+	nameAR : 'مراكش',
+	name : "Marrakech"
+	},
+	{
+	nameAR : 'مكناس',
+	name : "Meknès"
+	},
+	{
+	nameAR : 'الناظور',
+	name : "Nador"
+	},
+	{
+	nameAR : 'ورزازات',
+	name : "Ouarzazate"
+	},
+	{
+	nameAR : 'وجدة',
+	name : "Oujda-Angad"
+	},
+	{
+	nameAR : 'الرباط',
+	name : "Rabat"
+	},
+	{
+	nameAR : 'آسفي',
+	name : "Safi"
+	},
+	{
+	nameAR : 'طنجة',
+	name : "Tanger-Assilah"
+	},
+	{
+	nameAR : 'طرفاية',
+	name : "Tarfaya"
+	},
+	{
+	nameAR : 'تازة',
+	name : "Taza"
+	},
+	{
+	nameAR : 'تطوان',
+	name : "Tétouan"
+	},
+	{
+	nameAR : 'زاكورة',
+	name : "Zagora"
+	}
+];
